15a3beb1599a3e6e27bda3f0b096db84a11cbe44
Sam Bragg <sam.bragg@gmail.com>
2020-10-01 15:00:05 +0200

    Selenium cookies to Requests session

--- tradestation_client.py
+++ tradestation_client.py
@@ -5,6 +5,7 @@ import time
 
 from oauthlib.common import to_unicode
 from requests_oauthlib import OAuth2Session
+from requests import Session
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -18,23 +19,18 @@ class Tradestation:
     def __init__(self, username, password, client_id, client_secret, otp_secret=None, headless=True):
         self.username = username
         self.password = password
-        options = webdriver.ChromeOptions()
-        options.headless = headless
-        self.browser = webdriver.Chrome(options=options)
+        self.session = Session()
         self.otp_secret = otp_secret
+        self.headless = headless
 
         self.client_id = client_id
         self.client_secret = client_secret
- 
         self.api_url = 'https://api.tradestation.com/v2'
         self.authorisation_url = self.api_url + '/authorize'
         self.token_endpoint = self.api_url + '/Security/Authorize'
         self.redirect_uri = 'https://127.0.0.1/'
-
         self.oauth = OAuth2Session(self.client_id, redirect_uri=self.redirect_uri)
         self.token_filename =  os.path.join(os.path.dirname(__file__), 'tradestation-token.json')
-
-        self.accounts = self.get_user_accounts()
         
         if os.path.exists(self.token_filename):
             with open(self.token_filename) as token:
@@ -56,7 +52,6 @@ class Tradestation:
         self.oauth.register_compliance_hook('access_token_response', self.non_compliant_token)
         self.oauth.register_compliance_hook('refresh_token_response', self.non_compliant_token)
 
-        self.user_id = self.token['userid']
         self.accounts = self.get_user_accounts()
 
     def authorise(self):
@@ -86,8 +81,7 @@ class Tradestation:
         cash_transactions_url = f'https://clientcenter.tradestation.com/api/v1/Account/{account["Name"]}/{account["TypeDescription"]}/Trades/Cash/{date}/{date}'
         cash_transactions_params = '?page=1&pageSize=1000&orderBy=TradeDate&sortOrder=Ascending'
         
-        self.browser.get(cash_transactions_url + cash_transactions_params)
-        cash_transactions = self.browser_response_to_dict()['Results']
+        cash_transactions = self.session.get(cash_transactions_url + cash_transactions_params).json()['Results']
         
         for cash_transaction in cash_transactions:
             if self.include_transaction(cash_transaction):
@@ -104,8 +98,7 @@ class Tradestation:
         contracts_url = f'https://clientcenter.tradestation.com/api/v1/Account/{account["Name"]}/{account["TypeDescription"]}/Trades/Trades/{date}/{date}'
         contracts_params = '?page=1&pageSize=100&orderBy=ContractDescription&sortOrder=Ascending'
         
-        self.browser.get(contracts_url + contracts_params)
-        contracts_traded = self.browser_response_to_dict()['Results']
+        contracts_traded = self.session.get(contracts_url + contracts_params).json()['Results']
 
         for contract in contracts_traded:
             contract['AccountId'] = contract['AccountNo']
@@ -127,8 +120,7 @@ class Tradestation:
         purchase_sale_url = f'https://clientcenter.tradestation.com/api/v1/Account/{account["Name"]}/{account["TypeDescription"]}/Trades/PS/{date}/{date}'
         purchase_sale_params = '?page=1&pageSize=1000&orderBy=ContractDescription&sortOrder=Ascending'
         
-        self.browser.get(purchase_sale_url + purchase_sale_params)
-        closed_positions = self.browser_response_to_dict()['Results']
+        closed_positions = self.session.get(purchase_sale_url + purchase_sale_params).json()['Results']
 
         for closed_position in closed_positions:
             closed_position['TradeDate'] = date
@@ -164,23 +156,32 @@ class Tradestation:
         return not any(substr in description for substr in exclusions)
 
     def login(self):
+        options = webdriver.ChromeOptions()
+        options.headless = self.headless
+        browser = webdriver.Chrome(options=options)
         client_center = 'https://clientcenter.tradestation.com/'
 
-        self.browser.get(client_center)
-        uname_input = self.browser.find_element_by_id('username')
+        browser.get(client_center)
+        uname_input = browser.find_element_by_id('username')
         uname_input.send_keys(self.username)
         
-        pwd_input = self.browser.find_element_by_id('password')
+        pwd_input = browser.find_element_by_id('password')
         pwd_input.send_keys(self.password, Keys.RETURN)
 
-        otp_input = WebDriverWait(self.browser, 10).until(
+        otp_input = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.NAME, 'code'))
         )
         otp_input.send_keys(self.generate_otp(), Keys.RETURN)
 
-        return WebDriverWait(self.browser, 20).until(
+        WebDriverWait(browser, 60).until(
             EC.url_contains(client_center)
         )
+
+        cookies = browser.get_cookies()
+        for cookie in cookies:
+            self.session.cookies.set(cookie['name'], cookie['value'])
+        
+        browser.quit()
     
     def non_compliant_token(self, response):
         token = json.loads(response.text)
@@ -188,10 +189,6 @@ class Tradestation:
         fixed_token = json.dumps(token)
         response._content = to_unicode(fixed_token).encode("utf-8")
         return response
-    
-    def browser_response_to_dict(self):    
-        string = self.browser.find_element_by_xpath('/html/body/pre').text
-        return json.loads(string)
 
     def save_token(self, token):
         with open(self.token_filename, 'w') as output:
